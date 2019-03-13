42b969503452281943287048714e8e694f336588
liangtylerstone <liangtylerstone@users.noreply.github.com>
2019-03-13 14:51:03 +0800

    first

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,34 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Wolf> wolfArrayList =new ArrayList<Wolf>();
+       for (int i=0;i<3;i++){
+           wolfArrayList.add(new Wolf());
 
-        Lulu bLulu =new Lulu();
-        Fiora rFiora =new Fiora();
-        bLulu.showInfo();
-        System.out.println();
-        rFiora.showInfo();
-        System.out.println();
+       }
 
-        System.out.println("Lulu attack ");
-        rFiora.hurt(bLulu.attack());
-        bLulu.showInfo();
-        System.out.println();
-        rFiora.showInfo();
-        System.out.println();
-
-        System.out.println("Lulu use skillQ attack");
-        System.out.println();
-        rFiora.hurt(bLulu.skillQ());
-        bLulu.showInfo();
-        System.out.println();
-        rFiora.showInfo();
+//        Lulu bLulu =new Lulu();
+//        Fiora rFiora =new Fiora();
+//        bLulu.showInfo();
+//        System.out.println();
+//        rFiora.showInfo();
+//        System.out.println();
+//
+//        System.out.println("Lulu attack ");
+//        rFiora.hurt(bLulu.attack());
+//        bLulu.showInfo();
+//        System.out.println();
+//        rFiora.showInfo();
+//        System.out.println();
+//
+//        System.out.println("Lulu use skillQ attack");
+//        System.out.println();
+//        rFiora.hurt(bLulu.skillQ());
+//        bLulu.showInfo();
+//        System.out.println();
+//        rFiora.showInfo();
 
     }
 }
